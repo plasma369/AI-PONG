bd9d861867f54236c8bfe79a882c51168e51fb6c
plasma369 <76848965+plasma369@users.noreply.github.com>
2021-08-05 18:50:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25)
     text("Game Over!☹☹",width/2,height/2);
-    text("Reload The Page!",width/2,height/2+30)
+    text("Reload The Page to restart",width/2,height/2+30)
     noLoop();
     pcscore = 0;
 }
